e09356f9bd68ce87b7c3a2f6bb96521fb7b2780c
Nimasha Gamage <nimasha.piyumini@gmail.com>
2025-05-04 16:51:12 +0530

    Add vercel configs for both frontend and backend - 6

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -10,6 +11,7 @@ const favoriteRoutes = require('./routes/favoriteRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev')); // Logs requests
 
 // Healthcheck route
 app.get('/', (req, res) => {
