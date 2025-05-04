dd2cb47bf66ec8927f9f22f486ae6ff91e30e522
Nimasha Gamage <nimasha.piyumini@gmail.com>
2025-05-04 16:44:02 +0530

    Add vercel configs for both frontend and backend - 5

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://globe-vista-backend.vercel.app/api',
 });
 
 // Add token to headers if available
