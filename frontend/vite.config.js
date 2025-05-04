ea2e3e6a3647f47627e6f6494c54758fbd859430
Nimasha Gamage <nimasha.piyumini@gmail.com>
2025-05-04 11:08:43 +0530

    Update vercel config file - 10

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
-  base: '', // ✅ Set to empty string for correct asset loading on Vercel root
-  build: {
-    outDir: 'dist',
-  },
-});
+  base: '/'
+})
